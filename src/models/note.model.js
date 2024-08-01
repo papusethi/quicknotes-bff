27d6a60f84af8feb92810ad1a89e2df1edf799ff
Papu Sethi <mrpapur2001@gmail.com>
2024-08-01 23:05:36 +0530

    Completed adding color attribute support for note

--- src/models/note.model.js
+++ src/models/note.model.js
@@ -2,11 +2,12 @@ import { model, Schema } from "mongoose";
 
 const noteSchema = new Schema(
   {
+    userId: { type: String, required: true },
     title: { type: String, required: true },
     description: { type: String },
     tags: { type: [String], default: [] },
     isPinned: { type: Boolean, default: false },
-    userId: { type: String, required: true }
+    color: { type: String, default: "#FFFFFF" }
   },
   { timestamps: true }
 );
