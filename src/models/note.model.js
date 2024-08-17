c95fd898b3c2b8349f83efba251d5dc6bca029e5
Papu Sethi <mrpapur2001@gmail.com>
2024-08-18 02:21:57 +0530

    Completed updating of color property default value to null

--- src/models/note.model.js
+++ src/models/note.model.js
@@ -3,12 +3,12 @@ import { model, Schema } from "mongoose";
 const noteSchema = new Schema(
   {
     userId: { type: String, required: true },
-    title: { type: String, required: true },
+    title: { type: String },
     description: { type: String },
     tags: { type: [String], default: [] },
     isPinned: { type: Boolean, default: false },
     isArchived: { type: Boolean, default: false },
-    color: { type: String, default: "#FFFFFF" }
+    color: { type: String, default: null }
   },
   { timestamps: true }
 );
