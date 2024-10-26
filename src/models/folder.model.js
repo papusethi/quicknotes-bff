1536b6daf34c276ec1012fef1c6e6574783f5b38
Papu Sethi <mrpapur2001@gmail.com>
2024-10-26 15:32:07 +0530

    Completed updating schema for label and folder name

--- src/models/folder.model.js
+++ src/models/folder.model.js
@@ -3,7 +3,7 @@ import { model, Schema } from "mongoose";
 const folderSchema = new Schema(
   {
     userId: { type: String, required: true },
-    name: { type: String }
+    name: { type: String, required: true, unique: true }
   },
   { timestamps: true }
 );
